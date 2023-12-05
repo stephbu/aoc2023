6d0327534e4f7e85aaec7c450590db6cb86dd418
Steve Butler <stephbu@gmail.com>
2023-12-05 12:41:44 -0800

    Code Cleanup

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -169,15 +169,3 @@ long GetValueOrDefault(Mapping[] maps, long from)
     // identity transformation for missing ranges
     return from;
 }
-
-struct StaticMaps()
-{
-    public Mapping[] SeedToSoil;
-    public Mapping[] SoilToFertilizer;
-    public Mapping[] FertilizerToWater;
-    public Mapping[] WaterToLight;
-    public Mapping[] LightToTemperature;
-    public Mapping[] TemperatureToHumidity;
-    public Mapping[] HumidityToLocation;
-}
-
---
+++ Day5/StaticMaps.cs
@@ -0,0 +1,10 @@
+struct StaticMaps()
+{
+    public Mapping[] SeedToSoil;
+    public Mapping[] SoilToFertilizer;
+    public Mapping[] FertilizerToWater;
+    public Mapping[] WaterToLight;
+    public Mapping[] LightToTemperature;
+    public Mapping[] TemperatureToHumidity;
+    public Mapping[] HumidityToLocation;
+}
